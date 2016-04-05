a7b1bbf323281fe66958ebf49109aa5eb8eb27ad
youxin.zhuo <zhuoyouxin@zhuoyouxindeMac-mini.local>
2016-04-05 20:48:55 +0800

    update ZYXTouchTracker

--- ZYXTouchTracker/ZYXTouchTracker/ZYXDrawView.m
+++ ZYXTouchTracker/ZYXTouchTracker/ZYXDrawView.m
@@ -9,12 +9,14 @@
 #import "ZYXDrawView.h"
 #import "ZYXLine.h"
 
-@interface ZYXDrawView ()
+@interface ZYXDrawView () <UIGestureRecognizerDelegate>
 
 @property (nonatomic, strong) NSMutableDictionary *lineInprogress;
 @property (nonatomic, strong) NSMutableArray *finishedLines;
+@property (nonatomic, strong) UIPanGestureRecognizer *moveRecognizer;
 
 @property (nonatomic, weak) ZYXLine *selectedLine;
+
 
 @end
 
@@ -44,8 +46,17 @@
         UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
         tapRecognizer.delaysTouchesBegan = YES;
         [tapRecognizer requireGestureRecognizerToFail:doubleTabRecognizer];
+        
+        UILongPressGestureRecognizer *pressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPress:)];
+        
         [self addGestureRecognizer:tapRecognizer];
         [self addGestureRecognizer:doubleTabRecognizer];
+        [self addGestureRecognizer:pressRecognizer];
+        
+        self.moveRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveLine:)];
+        self.moveRecognizer.delegate = self;
+        self.moveRecognizer.cancelsTouchesInView = NO;
+        [self addGestureRecognizer:self.moveRecognizer];
     }
     
     return self;
@@ -111,6 +122,12 @@
     return nil;
 }
 
+- (BOOL)menuVisible
+{
+    UIMenuController *menu = [UIMenuController sharedMenuController];
+    return menu.menuVisible;
+}
+
 - (NSString *)linesArchivePath
 {
     NSString *documentPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).lastObject;
@@ -122,6 +139,7 @@
 {
     [NSKeyedArchiver archiveRootObject:self.finishedLines toFile:[self linesArchivePath]];
 }
+
 
 #pragma mark - GestureRecognizer Method
 - (void)doubleTap:(UITapGestureRecognizer *)gr
@@ -138,12 +156,100 @@
     CGPoint point = [gr locationInView:self];
     self.selectedLine = [self lineAtPoint:point];
     
+    if (self.selectedLine)
+    {
+        [self becomeFirstResponder];
+        
+        UIMenuController *menu = [UIMenuController sharedMenuController];
+        UIMenuItem *deleteItem = [[UIMenuItem alloc] initWithTitle:@"Delete" action:@selector(deleteLine:)];
+        
+        menu.menuItems = @[deleteItem];
+        [menu setTargetRect:CGRectMake(point.x, point.y, 2, 2) inView:self];
+        [menu setMenuVisible:YES animated:YES];
+    }
+    else
+    {
+        [[UIMenuController sharedMenuController] setMenuVisible:NO animated:YES];
+    }
+    
     [self setNeedsDisplay];
+}
+
+- (void)longPress:(UILongPressGestureRecognizer *)gr
+{
+    if (gr.state == UIGestureRecognizerStateBegan)
+    {
+        CGPoint point = [gr locationInView:self];
+        self.selectedLine = [self lineAtPoint:point];
+        
+        if (self.selectedLine)
+        {
+            [self.lineInprogress removeAllObjects];
+        }
+    }
+    else if (gr.state == UIGestureRecognizerStateEnded)
+    {
+        self.selectedLine = nil;
+    }
+    
+    [self setNeedsDisplay];
+}
+
+- (void)moveLine:(UIPanGestureRecognizer *)gr
+{
+    if (!self.selectedLine || [self menuVisible])
+    {
+        return;
+    }
+    
+    if (gr.state == UIGestureRecognizerStateChanged)
+    {
+        //手指拖动的距离
+        CGPoint translation = [gr translationInView:self];
+        
+        CGPoint begin = self.selectedLine.begin;
+        CGPoint end = self.selectedLine.end;
+        begin.x += translation.x;
+        begin.y += translation.y;
+        end.x += translation.x;
+        end.y += translation.y;
+        
+        self.selectedLine.begin = begin;
+        self.selectedLine.end = end;
+        [self setNeedsDisplay];
+        
+        [gr setTranslation:CGPointZero inView:self];
+    }
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (void)deleteLine:(id)sender
+{
+    [self.finishedLines removeObject:self.selectedLine];
+    [self setNeedsDisplay];
+}
+
+#pragma mark - GestureRecognizer Delegate
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    if (gestureRecognizer == self.moveRecognizer)
+    {
+        return YES;
+    }
+    return NO;
 }
 
 #pragma mark - Touches Action
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
+    if ([self menuVisible])
+    {
+        return;
+    }
     for (UITouch *t in touches)
     {
         CGPoint location = [t locationInView:self];
@@ -161,6 +267,10 @@
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
+    if ([self menuVisible])
+    {
+        return;
+    }
     for (UITouch *t in touches)
     {
         NSValue *key = [NSValue valueWithNonretainedObject:t];
@@ -174,7 +284,10 @@
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    
+    if ([self menuVisible])
+    {
+        return;
+    }
     for (UITouch *t in touches)
     {
         NSValue *key = [NSValue valueWithNonretainedObject:t];
@@ -188,6 +301,10 @@
 
 - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
+    if ([self menuVisible])
+    {
+        return;
+    }
     for (UITouch *t in touches)
     {
         NSValue *key = [NSValue valueWithNonretainedObject:t];
