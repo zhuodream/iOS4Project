ac21e7a686f4c78c1ac890ba10b851001b464c62
zhuodream <278941654@qq.com>
2016-04-14 02:41:23 +0800

    update homepnoNerd

--- ZYXHyponNerd/ZYXHyponNerd/ZYXHypnoNerdViewController.m
+++ ZYXHyponNerd/ZYXHyponNerd/ZYXHypnoNerdViewController.m
@@ -11,6 +11,8 @@
 
 @interface ZYXHypnoNerdViewController () <UITextFieldDelegate>
 
+@property (nonatomic, weak) UITextField *textField;
+
 @end
 
 @implementation ZYXHypnoNerdViewController
@@ -36,14 +38,25 @@
     
     self.view = backgroundView;
     
-    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    CGRect textFieldRect = CGRectMake(40, -20, 240, 30);
     UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
     
     textField.borderStyle = UITextBorderStyleRoundedRect;
     textField.placeholder = @"Hypnotize me";
     textField.returnKeyType = UIReturnKeyDone;
     textField.delegate = self;
+    self.textField = textField;
     [backgroundView addSubview:textField];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [UIView animateWithDuration:2.0 delay:0.0 usingSpringWithDamping:0.25 initialSpringVelocity:0.0 options:0 animations:^{
+        CGRect frame = CGRectMake(40, 70, 240, 40);
+        self.textField.frame = frame;
+    } completion:nil];
 }
 
 - (void)viewDidLoad {
@@ -83,6 +96,24 @@
         messageLabel.frame = frame;
         
         [self.view addSubview:messageLabel];
+        
+        messageLabel.alpha = 0.0;
+        [UIView animateWithDuration:0.5 delay:0.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+            messageLabel.alpha = 1.0;
+        } completion:nil];
+        
+        [UIView animateKeyframesWithDuration:1.0 delay:0.0 options:0 animations:^{
+            [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.8 animations:^{
+                messageLabel.center = self.view.center;
+            }];
+            [UIView addKeyframeWithRelativeStartTime:0.8 relativeDuration:0.2 animations:^{
+                int x = arc4random() % width;
+                int y = arc4random() % height;
+                messageLabel.center = CGPointMake(x, y);
+            }];
+        } completion:^(BOOL finished){
+            NSLog(@"Animation finished");
+         }];
         
         //添加运动效果,只能在真实设备上测试
         UIInterpolatingMotionEffect *motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
